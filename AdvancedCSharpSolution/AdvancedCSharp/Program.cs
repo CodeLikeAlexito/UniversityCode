5804de6293d64bf6a99c28a4b355e6ee6164c01a
Alex <aceivanov0@gmail.com>
2020-05-26 21:33:58 +0300

    First commit

---
+++ AdvancedCSharpSolution/AdvancedCSharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AdvancedCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object obj = "Mosh";
+            Console.WriteLine(obj.GetHashCode());
+
+            dynamic excelObject = "mosh";
+            Console.WriteLine(excelObject.Optimize());
+            //excelObject.Optimize();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
